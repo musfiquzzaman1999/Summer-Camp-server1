6dad9a3126c7a081e51feeb94f5a02448be0f57f
unknown <musfiquzzama1999@gmail.com>
2023-06-10 02:46:18 +0600

    jwt problems solve

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'Unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'Unauthorized access' });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q5q1wsb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -21,33 +39,39 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const classCollection = client.db("summerCamp").collection("classes");
-    const usersCollection = client.db("summerCamp").collection("users");
+    const classCollection = client.db('summerCamp').collection('classes');
+    const usersCollection = client.db('summerCamp').collection('users');
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    });
 
-    app.get('/classes', async(req, res) =>{
+    app.get('/classes', async (req, res) => {
+      try {
         const result = await classCollection.find().toArray();
         res.send(result);
-    })
+      } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
 
-    app.get('/classes/:email', (req, res) => {
+    app.get('/classes/:email', async (req, res) => {
+      try {
         const email = req.params.email;
-        
-        
-    
-        classCollection.find({ instructorEmail: email }).toArray().then((classes) => {
-          res.send(classes);
-        });
-      });
+        const classes = await classCollection.find({ instructorEmail: email }).toArray();
+        res.send(classes);
+      } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
 
     app.get('/popular-classes', async (req, res) => {
       try {
-        const popularClasses = await classCollection
-          .find()
-          .sort({ students: -1 })
-          .limit(6)
-          .toArray();
-
+        const popularClasses = await classCollection.find().sort({ students: -1 }).limit(6).toArray();
         res.json(popularClasses);
       } catch (error) {
         console.error('Error:', error);
@@ -55,92 +79,141 @@ async function run() {
       }
     });
 
-
     app.get('/instructors', async (req, res) => {
-        try {
-          const popularInstructors = await classCollection
-            .aggregate([
-              {
-                $group: {
-                  _id: "$instructorEmail",
-                  totalStudents: { $sum: "$students" },
-                  instructor: { $first: "$$ROOT" },
-                },
-              },
-              { $sort: { totalStudents: -1 } },
-              
-              {
-                $project: {
-                  _id: 0,
-                  name: "$instructor.instructorName",
-                  email: "$instructor.instructorEmail",
-                  totalStudents: 1,
-                  thumbnail: "$instructor.instructorPicture",
-                },
-              },
-            ])
-            .toArray();
-      
-          res.json(popularInstructors);
-        } catch (error) {
-          console.error('Error:', error);
-          res.status(500).json({ error: 'Internal server error' });
-        }
-      })
+      try {
+        const popularInstructors = await classCollection.aggregate([
+          {
+            $group: {
+              _id: '$instructorEmail',
+              totalStudents: { $sum: '$students' },
+              instructor: { $first: '$$ROOT' },
+            },
+          },
+          { $sort: { totalStudents: -1 } },
+          {
+            $project: {
+              _id: 0,
+              name: '$instructor.instructorName',
+              email: '$instructor.instructorEmail',
+              totalStudents: 1,
+              thumbnail: '$instructor.instructorPicture',
+            },
+          },
+        ]).toArray();
+
+        res.json(popularInstructors);
+      } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
 
     app.get('/popular-instructors', async (req, res) => {
-        try {
-          const popularInstructors = await classCollection
-            .aggregate([
-              {
-                $group: {
-                  _id: "$instructorEmail",
-                  totalStudents: { $sum: "$students" },
-                  instructor: { $first: "$$ROOT" },
-                },
-              },
-              { $sort: { totalStudents: -1 } },
-              { $limit: 6 },
-              {
-                $project: {
-                  _id: 0,
-                  name: "$instructor.instructorName",
-                  email: "$instructor.instructorEmail",
-                  totalStudents: 1,
-                  thumbnail: "$instructor.instructorPicture",
-                },
-              },
-            ])
-            .toArray();
-      
-          res.json(popularInstructors);
-        } catch (error) {
-          console.error('Error:', error);
-          res.status(500).json({ error: 'Internal server error' });
-        }
-      })
-     
+      try {
+        const popularInstructors = await classCollection.aggregate([
+          {
+            $group: {
+              _id: '$instructorEmail',
+              totalStudents: { $sum: '$students' },
+              instructor: { $first: '$$ROOT' },
+            },
+          },
+          { $sort: { totalStudents: -1 } },
+          { $limit: 6 },
+          {
+            $project: {
+              _id: 0,
+              name: '$instructor.instructorName',
+              email: '$instructor.instructorEmail',
+              totalStudents: 1,
+              thumbnail: '$instructor.instructorPicture',
+            },
+          },
+        ]).toArray();
 
-      app.get('/users',  async (req, res) => {
+        res.json(popularInstructors);
+      } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+
+    app.get('/users', async (req, res) => {
+      try {
         const result = await usersCollection.find().toArray();
         res.send(result);
-      });
+      } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
 
-      app.post('/users', async (req, res) => {
+    app.post('/users', verifyJWT, async (req, res) => {
+      try {
         const user = req.body;
-        const query = { email: user.email }
+        const query = { email: user.email };
         const existingUser = await usersCollection.findOne(query);
-      
+
         if (existingUser) {
-          return res.send({ message: 'user already exists' })
+          return res.send({ message: 'User already exists' });
         }
-      
+
         const result = await usersCollection.insertOne(user);
         res.send(result);
-      });
+      } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'Forbidden message' });
+      }
+      next();
+    };
+
+    app.get('/users/admin/:email', verifyJWT,verifyAdmin, async (req, res) => {
+      try {
+        const email = req.params.email;
+
+        if (req.decoded.email !== email) {
+          return res.send({ admin: false });
+        }
+
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        const result = { admin: user?.role === 'admin' };
+        res.send(result);
+      } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: 'admin',
+          },
+        };
+
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
